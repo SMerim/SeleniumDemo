4afb6ba2ef72f05cdfa966cbc28ae1fd4211b1aa
SMerim <smerim2110@gmail.com>
2024-08-16 22:07:28 -0400

    First commit for this project

---
+++ src/com/practice/Task1.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		/*
+		 * HW1: Amazon Page Title Verification: Open chrome browser Go to
+		 * “https://www.amazon.com/” Verify Title “Amazon.com: Online Shopping for
+		 * Electronics, Apparel, Computers, Books, DVDs & more” is displayed End the
+		 * Session (close the browser).
+		 */
+
+	}
+
+}
